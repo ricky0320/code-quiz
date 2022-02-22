e39fc80f06c5da7e45fce1cfe171b7446160c081
Ricky Leung <ricxx0320@gmail.com>
2022-02-22 00:38:48 -0800

    modified questions, script functions

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// Questions
+var quizQuestions = [
+  // quiz 1, index 0
+  {Question: "Which of the following is NOT a loop used in coding?",
+    a: "a. for loop",
+    b: "b. while loop",
+    c: "c. jiffy loop",
+
+    // let c be answer
+    ans: "c. jiffy loop"
+  },
+  // quiz 2, index 1
+  {Question: "which of the following is not a operator used in JavaScript?",
+    a: "a. <",
+    b: "b. ||",
+    c: "c. ====",
+    // let c be answer
+    ans: "c. ===="
+  },
+  // quiz 3, index 2
+  {Question: "which of the following is not a HTML element tag?",
+    a: "a. addition",
+    b: "b. button",
+    c: "c. div",
+    // let a be answer
+    ans: "a. addition"
+  }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
-
 var timerEl = document.getElementById('countdown');
 var buttonEl = document.getElementById("start-btn");
 var submitButton = document.getElementById('submit');
+var showAns = document.getElementById('showAns');
+
 
 // target buttons a,b,c for answer choices
 var aButton = document.getElementById("a")
@@ -13,76 +14,86 @@ bButton.style.display ="none"
 cButton.style.display ="none"
 
 var Qindex = 0
+var timer = quizQuestions.length * 20;
 
 
-var createQuiz = function() {
+
+var StartQuiz = function() {
   
   // when click start quiz button, hide quiz heading
-  document.getElementById("quiz-heading").style.display = "none"
+  document.getElementById("quiz-heading").style.display = "none";
+
   // when user click start quiz button,// hide button
-  buttonEl.style.display = "none"
-  //display first question
+  buttonEl.style.display = "none";
+
+  // hide showAns
+  showAns.style.display = "none";
+
+  // start timer
+  timerEl = timer;
+
+  // pull up quiz questions
+  getQuizQuestion();
+}
+
+
+
+function getQuizQuestion() {
+  //display question
   document.getElementById("question").textContent = quizQuestions[Qindex].Question
+
   //display button
   aButton.style.display ="block"
   bButton.style.display ="block"
   cButton.style.display ="block"
+
   // add answers to buttons
   aButton.textContent = quizQuestions[Qindex].a
   bButton.textContent = quizQuestions[Qindex].b
   cButton.textContent = quizQuestions[Qindex].c
-  
-  
 
-}
+
+};
+
+
+
 
 // function checkAns() {}
 var checkAns = function() {
   if (quizQuestions[Qindex].ans === this.textContent) {
-    console.log("the answer is correct")
+    showAns.textContent = "Correct."
   }
   else{
-    console.log("answer is wrong")
+    showAns.textContent = "Wrong."
   }
+  // next question
+  Qindex++;
+
+  // check if there is a next question
+  if (Qindex === quizQuestions.length) {
+    EndQuiz();
+  } else {
+    getQuizQuestion();
+  }
+};
+
+function EndQuiz() {
+  // stop timer
+  clearInterval(timer);
+
+  
 }
-
-
 
 // click button to check answer
 aButton.addEventListener('click', checkAns)
 bButton.addEventListener('click', checkAns)
 cButton.addEventListener('click', checkAns)
 
+// start quiz when clicking this button
 buttonEl.addEventListener('click',createQuiz);
 
 
 // on submit, show results
 // submitButton.addEventListener('click', showResults);
 
-var quizQuestions = [
-  // quiz 1, index 0
-  {Question: "Which of the following is NOT a loop used in coding?",
-    a: "a. for loop",
-    b: "b. while loop",
-    c: "c. jiffy loop",
 
-    // let c be answer
-    ans: "c. jiffy loop"
-  },
-  // quiz 2, index 1
-  {Question: "which of the following is not a operator used in JavaScript?",
-    a: "a. <",
-    b: "b. ||",
-    c: "c. ====",
-    // let c be answer
-    ans: "c. ===="
-  },
-  // quiz 3, index 2
-  {Question: "which of the following is not a HTML element tag?",
-    a: "a. addition",
-    b: "b. button",
-    c: "c. div",
-    // let a be answer
-    ans: "a. addition"
-  }
-]
